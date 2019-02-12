215a6c013cc3c250b320008044e9e89823c641cd
s150904 <s150904@berufskolleg-borken.de>
2019-02-12 13:28:16 +0100

    ...

---
+++ server.js
@@ -0,0 +1,7 @@
+// Das ist ein einzeiliger Kommentar
+/* Das ist ein mehrzeiliger Kommentar */
+
+// Der Bodyparser bereitet die Daten aus dem HTML-Formular vor der Übergabe an die server.js auf.
+// Der Bodyparser muss über das Terminal installiert werden.
+// npm install body-parser --save
+const body = require('body-praser')
