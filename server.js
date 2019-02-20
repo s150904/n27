554cbc12911440999e7020e7ac649dde95a4d7f5
s150904 <s150904@berufskolleg-borken.de>
2019-02-20 10:57:11 +0100

    ...

--- server.js
+++ server.js
@@ -4,4 +4,20 @@
 // Der Bodyparser bereitet die Daten aus dem HTML-Formular vor der Übergabe an die server.js auf.
 // Der Bodyparser muss über das Terminal installiert werden.
 // npm install body-parser --save
-const body = require('body-praser')
+const bodyParser = require('body-parser')
+
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(express.static('public'))
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Der Server ist erfolgreich gestartet auf Port %s', server.address().port)
+})
+
+app.get('/',())
